ba40a4546fd2596970366c56c4c46e7c35c6c6d5
Mark Taylor <tardinha@gmail.com>
2015-05-29 15:14:35 +1000

    Update to current version of FullCalendar plugin

--- code/FullCalendarControllerExtension.php
+++ code/FullCalendarControllerExtension.php
@@ -7,86 +7,102 @@
  */
 class FullCalendarControllerExtension extends Extension {
 
-	private static $allowed_actions = array(
-		'full',
-		'eventsdata'
-	);
+  private static $allowed_actions = array(
+    'full',
+    'eventsdata'
+  );
 
 
-	public function onAfterInit(){
-		$request = $this->owner->getRequest();
+  public function onAfterInit(){
+    $request = $this->owner->getRequest();
 
-		if(!$request->param('Action') && $this->owner->data()->UseFullCalendar){
-			return $this->owner->redirect($this->owner->Link('full'));
-		}
-	}
+    if(!$request->param('Action') && $this->owner->data()->UseFullCalendar){
+      return $this->owner->redirect($this->owner->Link('full'));
+    }
+  }
 
-	/**
-	 * @return full calendar view
-	 */
-	public function full() {
-		Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery.js');
-		Requirements::javascript('fullcalendar/thirdparty/jquery-fullcalendar/fullcalendar.js');
-		Requirements::javascript('fullcalendar/javascript/FullCalendar.js');
-		Requirements::css('fullcalendar/thirdparty/jquery-fullcalendar/fullcalendar.css');
+  /**
+   * @return full calendar view
+   */
+  public function full() {
 
-		$basicAgenda = 'agenda';
+    Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery.js');
 
-		switch ($this->owner->data()->DefaultView) {
-			case 'today':
-				$view = $basicAgenda . 'Day';
-			case 'week':
-				$view = $basicAgenda . 'Week';
-				break;
-			default:
-				$view = 'month';
-				break;
-		}
-		return $this->owner->customise(array(
-			'FullCalendarView' => $view
-		));
-	}
+    Requirements::javascript('fullcalendar/thirdparty/fullcalendar-2.3.1/lib/moment.min.js');
+    Requirements::javascript('fullcalendar/thirdparty/fullcalendar-2.3.1/fullcalendar.min.js');
 
-	/**
-	 * Handles returning the JSON events data for a time range.
-	 *
-	 * @param  SS_HTTPRequest $request
-	 * @return SS_HTTPResponse
-	 */
-	public function eventsdata($request) {
-		$start = $request->getVar('start');
-		$end   = $request->getVar('end');
+    Requirements::javascript('fullcalendar/javascript/FullCalendar.js');
 
-		// for testing
-		if(!$end){
-			$end = '2013-12-12';
-		}
+    Requirements::css('fullcalendar/thirdparty/fullcalendar-2.3.1/fullcalendar.min.css');
 
-		$events = $this->owner->data()->getEventList(
-	      	sfDate::getInstance($start)->date(),
-	      	sfDate::getInstance($end)->date(),
-	      	null,
-	      	null
-	    );
+    $basicAgenda = 'agenda';
 
-		$result = array();
+    switch ($this->owner->data()->DefaultView) {
+    case 'today':
+      $view = $basicAgenda . 'Day';
+    case 'week':
+      $view = $basicAgenda . 'Week';
+      break;
+    default:
+      $view = 'month';
+      break;
+    }
+    return $this->owner->customise(array(
+        'FullCalendarView' => $view
+      ));
+  }
 
-		if ($events) foreach ($events as $event) {
-			$result[] = array(
-				'id'        => $event->ID,
-				'title'     => $event->getTitle(),
-				'start'     => strtotime("$event->StartDate $event->StartTime"),
-				'end'       => strtotime("$event->EndDate $event->EndTime"),
-				'startTime' => $event->getFormattedStartTime(),
-          		'endTime'   => $event->getFormattedEndTime(),
-				'allDay'    => (bool) $event->AllDay,
-				'url'       => $event->Link(),
-				//'className' => $event->Event()->Parent()->ElementName());
-			);
-		}
+  /**
+   * Handles returning the JSON events data for a time range.
+   *
+   * @param  SS_HTTPRequest $request
+   * @return SS_HTTPResponse
+   */
+  public function eventsdata($request) {
+    $start = $request->getVar('start');
+    $end   = $request->getVar('end');
 
-		$this->owner->getRequest()->addHeader('Content-Type', 'application/json');
-		return Convert::array2json($result);
-	}
+    // for testing
+    if(!$end){
+      $end = '2015-12-12';
+    }
+
+    $events = $this->owner->data()->getEventList(
+      sfDate::getInstance($start)->date(),
+      sfDate::getInstance($end)->date(),
+      null,
+      null
+    );
+
+    $result = array();
+
+    if ($events) foreach ($events as $event) {
+        $result[] = array(
+          'id'        => $event->ID,
+          'title'     => $event->getTitle(),
+/*
+          'start'     => strtotime("$event->StartDate $event->StartTime"),
+          'end'       => strtotime("$event->EndDate $event->EndTime"),
+*/
+          'start'     => $event->MicroformatStart(),
+          'end'       => $event->MicroformatEnd(),
+          'startTime' => $event->getFormattedStartTime(),
+          'endTime'   => $event->getFormattedEndTime(),
+          'allDay'    => (bool) $event->AllDay,
+          'url'       => $event->Link(),
+          //'className' => $event->Event()->Parent()->ElementName());
+        );
+      }
+
+/*
+    $this->owner->getRequest()->addHeader('Content-Type', 'application/json');
+    return Convert::array2json($result);
+*/
+
+    $response = new SS_HTTPResponse(Convert::array2json($result));
+    $response->addHeader('Content-Type', 'application/json');
+    return $response;
+
+  }
 
 }
